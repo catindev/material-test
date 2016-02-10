d8d9a30260a70444a9294c883357602fd455be7f
Vladimir Titskiy <Vladimir_Titskiy@epam.com>
2016-02-10 12:17:03 +0600

    form sections

--- src/components/requests/new-request/new.controller.js
+++ src/components/requests/new-request/new.controller.js
@@ -2,6 +2,20 @@ function Controller($mdDialog) {
 	this.close = function() {
 		return $mdDialog.hide();
 	}
+
+	this.data = {
+		one: {
+			name: 'Пятилова',
+			n: 'Бизнес-направление',
+			b: 'Блок развития'
+		},
+
+		two: {
+			name: 'Новиков',
+			n: 'Бизнес-направление',
+			b: 'Блок развития'
+		}
+	}
 }
 
 export default Controller;
